bedacabfc5b7b2362a0e056f128cd0b8891cbf9a
Lukáš Mačuda <lukas.macuda@gmail.com>
2018-07-06 08:16:53 +0200

    fixed key in _setData

--- src/base.js
+++ src/base.js
@@ -106,7 +106,7 @@ export default class BaseModel {
             if (data[property.name] === undefined) {
                 return;
             }
-            this[property] = property.cast(data[property.name]);
+            this[property.name] = property.cast(data[property.name]);
         });
     }
 
